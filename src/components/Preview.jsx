8fa4a2b294196f794fd9dca9360adc9e11d533f6
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2024-05-09 15:19:29 +0100

    only commit

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -3,7 +3,7 @@ import ScrollableImage from "./ScrollableImage"
 const Preview = () => {
   return (
     <div className="bg-primary text-base-100 text-center">
-      <div className="grid gap-4 align-element pt-10 pb-24 2xl:pb-80">
+      <div className="grid gap-4 align-element pt-10 pb-24 md:pt-20 md:pb-48 2xl:pt-10 2xl:pb-80">
         <div className="text-3xl font-medium">Here's a preview of our Talent Profile</div>
         <div className="md:mx-48">
           Our profiles include the most essential information to help you decide whether to make contact.
@@ -11,7 +11,7 @@ const Preview = () => {
           each candidiate upfront.
         </div>
       </div>
-      <div>
+      <div className="2xl:grid 2xl:place-items-center">
         <ScrollableImage />
       </div>
     </div>
--- src/components/ScrollableImage.jsx
+++ src/components/ScrollableImage.jsx
@@ -4,10 +4,10 @@ import preview from '/home-talent-preview.jpeg';
 const ScrollableImage = () => {
   return (
     <div>
-      <div className='rounded-xl overflow-y-scroll max-w-screen-lg mx-auto active:border active:border-dashed active:border-black absolute w-4/5 h-72 2xl:left-[550px] 2xl:top-[4850px] lg:h-96 lg:top-[4550px] lg:left-36 md:top-auto -mt-16 md:left-24 top-auto left-[10vw]'>
+      <div className='rounded-xl overflow-y-scroll max-w-screen-lg mx-auto active:border active:border-dashed active:border-black absolute w-4/5 h-72 2xl:left-[550px] 2xl:top-auto lg:h-96 lg:top-[4550px] lg:left-36 md:top-auto -mt-16 2xl:-mt-56 md:left-24 top-auto left-[10vw]'>
         <img src={preview} alt="talent preview" />
       </div>
-      <div className='absolute mt-36 top-auto left-[45vw] lg:left-[50vw] lg:mt-48 text-black bg-base-200 rounded-full p-2'><FaMouse size={35} /></div>
+      <div className='absolute mt-36 top-auto left-[45vw] lg:left-[50vw] lg:mt-48 2xl:mt-16 text-black bg-base-200 rounded-full p-2'><FaMouse size={35} /></div>
     </div>
   )
 }
